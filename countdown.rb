62a1efc2e2bcda053e12131d74db2fa4d3dec793
Bruce Ruffin <bruffin1221@gmail.com>
2019-07-09 15:58:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+def countdown(number)
+  number=10
+  while number>0
+    puts "#{number} SECOND(S)!"
+    number-=1
+    end
+    "HAPPY NEW YEAR!"
+  end
 
-def countdown
-end
+ def countdown_with_sleep(5.seconds)
+   number=10
+   while number>0
+     puts "#{number} SECOND(S)!"
+     number-=1
+     end
+     "HAPPY NEW YEAR!"
+   end
